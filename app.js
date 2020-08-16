213654415cfb07bad4309377672d3bead962f8fa
Moddoo <>
2020-08-16 16:53:22 -0500

    heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const localStrategy = require("passport-local");
 const expressSession = require("express-session");
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 const { currentUser } = require("./middleware/log");
 
